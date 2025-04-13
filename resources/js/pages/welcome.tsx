fffee9610c194ae754de38dbdec5862b6394d499
Andrew Gilliland <andrewpgilliland@gmail.com>
2025-04-13 12:03:19 -0500

    doing something

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -4,9 +4,11 @@ import { Head, Link, usePage } from '@inertiajs/react';
 export default function Welcome() {
     const { auth } = usePage<SharedData>().props;
 
+    console.log(auth);
+
     return (
         <>
-            <Head title="Welcome">
+            <Head title="Home">
                 <link rel="preconnect" href="https://fonts.bunny.net" />
                 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
             </Head>
